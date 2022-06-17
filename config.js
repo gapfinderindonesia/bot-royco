9066bd869df57bd3f69592c42cab45c380ca455a
test <nirvanahandia@gmail.com>
2022-06-17 19:07:41 +0700

    tets

---
+++ config.js
@@ -0,0 +1,53 @@
+/**
+   * Create By M. Dandi Saputra
+   * Contact Me on wa.me/+6281352445558
+   * Follow https://github.com/gapfinderindonesia
+*/
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.APIs = {
+	zenz: 'https://zenzapis.xyz',
+}
+
+// Apikey Website Api
+global.APIKeys = {
+	'https://zenzapis.xyz': 'sonelstore',
+}
+
+// Other
+global.owner = ['6281352445558']
+global.premium = ['6281352445558']
+global.namaowner = ['M. Dandi Saputra']
+global.namabot = ['RoycoBot']
+global.packname = 'Created By Dandi Saputra'
+global.author = 'RoycoBot'
+global.sessionName = 'RoycoBot'
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '⭔'
+global.mess = {
+    success: '✓ Success',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    owner: 'Fitur Khusus Owner Bot',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: 'Loading...',
+    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+}
+global.limitawal = {
+    premium: "Infinity",
+    free: 100
+}
+global.thumb = fs.readFileSync('./lib/hisoka.jpg')
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
